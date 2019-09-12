70e6dfccf398ca00e41daa0b9f141a35beab1cce
ringo2455 <iby2455@gmail.com>
2019-09-12 18:00:14 +0900

    useReducer을 통한 성능최적화

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
+import { create } from 'istanbul-reports';
 
 function createBulkTodos() {
   const array = [];
@@ -15,9 +16,24 @@ function createBulkTodos() {
   return array;
 }
 
-const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+// action에는 type뿐만 아니라 비교, 업데이트, 수정 등 여러 작업을 할 수 있도록 도와주는 따른 정보들 또한 넣을 수 있다.
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
 
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
   // 고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
   const nextId = useRef(4);
@@ -28,20 +44,16 @@ const App = () => {
       text,
       checked: false,
     };
-    setTodos(todos => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
     nextId.current += 1;
   }, []);
 
   const onRemove = useCallback(id => {
-    setTodos(todos => todos.filter(todo => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
   }, []);
 
   const onToggle = useCallback(id => {
-    setTodos(todos =>
-      todos.map(todo =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-      ),
-    );
+    dispatch({ type: 'TOGGLE', id });
   }, []);
 
   return (
