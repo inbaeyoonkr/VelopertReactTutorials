491fd2a1eb50c38f44f4d32902b62129611cf3ed
ringo2455 <iby2455@gmail.com>
2019-09-12 19:22:19 +0900

    react-virtualized 라이브러리를 사용해서 List 와 ListItem 성능 최적화

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import styled from 'styled-components';
 import TodoListItem from './TodoListItem';
 
@@ -9,18 +10,35 @@ const TodoListBlock = styled.div`
 `;
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <TodoListBlock>
-      {todos.map(todo => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
-          key={todo.id}
           todo={todo}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
+  return (
+    <TodoListBlock>
+      <List
+        width={512} // 전체 크기
+        height={513} // 전체 높이
+        rowCount={todos.length} // 항목 개수
+        rowHeight={57} // 항목 높이
+        rowRenderer={rowRenderer} // 항목을 렌더링할 때 쓰는 함수
+        list={todos} // 배열
+        style={{ outline: 'none' }} // List에 기본 적용되는 outline 스타일 제거
+      />
     </TodoListBlock>
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -8,68 +8,81 @@ import {
 } from 'react-icons/md';
 
 const TodoListItemBlock = styled.div`
-  padding: 1rem;
-  display: flex;
-  align-items: center; /* 세로 중앙 정렬 */
-  &:nth-child(even) {
-    background: #f8f9fa;
-  }
-
-  .checkbox {
-    cursor: pointer;
-    flex: 1; /* 차지할 수 있는 영역 전부 차지 */
-    display: flex;
-    align-items: center;
-    svg {
-      font-size: 1.5rem;
-    }
-    .text {
-      margin-left: 0.5rem;
-      flex: 1;
-    }
-    /* 체크 되었을 때 보여줄 스타일  */
-    &.checked {
-      svg {
-        color: #22b8cf;
-      }
-      .text {
-        color: #adb5bd;
-        text-decoration: line-through;
-      }
-    }
-  }
-
-  .remove {
-    display: flex;
-    align-items: center;
-    font-size: 1.5rem;
-    color: #ff6b6b;
-    cursor: pointer;
-    &:hover {
-      color: #ff8787;
-    }
-  }
-
-  /* 엘리먼트 사이사이에 테두리를 넣어 줌 */
   & + & {
     border-top: 1px solid #dee2e6;
   }
+  &:nth-child(even) {
+    background: #f8f9fa;
+  }
+  .TodoListItem {
+    padding: 1rem;
+    display: flex;
+    align-items: center; /* 세로 중앙 정렬 */
+    &:nth-child(even) {
+      background: #f8f9fa;
+    }
+
+    .checkbox {
+      cursor: pointer;
+      flex: 1; /* 차지할 수 있는 영역 전부 차지 */
+      display: flex;
+      align-items: center;
+      svg {
+        font-size: 1.5rem;
+      }
+      .text {
+        margin-left: 0.5rem;
+        flex: 1;
+      }
+      /* 체크 되었을 때 보여줄 스타일  */
+      &.checked {
+        svg {
+          color: #22b8cf;
+        }
+        .text {
+          color: #adb5bd;
+          text-decoration: line-through;
+        }
+      }
+    }
+
+    .remove {
+      display: flex;
+      align-items: center;
+      font-size: 1.5rem;
+      color: #ff6b6b;
+      cursor: pointer;
+      &:hover {
+        color: #ff8787;
+      }
+    }
+  }
 `;
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <TodoListItemBlock>
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+    <TodoListItemBlock style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
 
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </TodoListItemBlock>
   );
 };
 
-export default React.memo(TodoListItem);
+// 자신의 props가 바뀌었을 때만 리렌더링하기 위해서다.
+// 성능 최적화
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
