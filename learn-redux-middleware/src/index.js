875b2ca6f1c15d9b168ef96e36f3bc9980cd402b
ringo2455 <iby2455@gmail.com>
2019-09-24 17:46:17 +0900

    counter에 redux-chunk 라이브러리 사용해보기

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-import loggerMiddleware from './lib/Middleware';
+import { createLogger } from 'redux-logger';
+import ReduxChunk from 'redux-thunk';
+const logger = createLogger();
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxChunk));
 
 ReactDOM.render(
   <Provider store={store}>
