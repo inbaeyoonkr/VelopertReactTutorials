8e1e068daf63fc609688efa789af0cdf7e55881c
ringo2455 <iby2455@gmail.com>
2019-10-28 19:43:48 +0900

    blog에 mongoDB 사용하기

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,21 +1,12 @@
-const Router = require('koa-router');
-const postCtrl = require('./posts.ctrl');
+import Router from 'koa-router';
+import * as postCtrl from './posts.ctrl';
 
 const posts = new Router();
-
-const printInfo = ctx => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
 
 posts.get('/', postCtrl.list);
 posts.post('/', postCtrl.write);
 posts.get('/:id', postCtrl.read);
 posts.delete('/:id', postCtrl.remove);
-posts.put('/:id', postCtrl.replace);
 posts.patch('/:id', postCtrl.update);
 
-module.exports = posts;
+export default posts;
--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,119 +1,55 @@
-let postId = 1; // id의 초깃값이다.
+import Post from '../../models/post';
 
-// posts 배열 초기 데이터
-const posts = [
-  {
-    id: 1,
-    title: '제목',
-    body: '내용',
-  },
-];
-
-/* 포스트 작성
+/*
 POST /api/posts
-{ title, body }
+{
+  title: '제목',
+  body: '내용',
+  tags: ['태그1', '태그2']
+}
 */
-exports.write = ctx => {
-  // REST API의 Request Body는 ctx.request.body에서 조회할 수 있다.
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = {
-    id: postId,
+export const write = async ctx => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
     title,
     body,
-  };
-  posts.push(post);
-  ctx.body = post;
+    tags
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
-/* 포스트 목록 조회
+/*
 GET /api/posts
 */
-exports.list = ctx => {
-  ctx.body = posts;
+export const list = async ctx => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
-/* 특정 포스트 조회
+/*
 GET /api/posts/:id
 */
-exports.read = ctx => {
+export const read = async ctx => {
   const { id } = ctx.params;
-  // 주어진 id 값으로 포스트를 찾는다
-  // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
-  // 비교할 때는 p.id 값을 문자열로 변경해야 한다.
-  const post = posts.find(post => post.id.toString() === id);
-  // 포스터가 없으면 오류를 반환한다.
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다',
-    };
-    return;
+  try {
+    const post = Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
 };
-
-/* 특정 포스트 제거
-DELETE /api/posts/:id
-*/
-exports.remove = ctx => {
-  const { id } = ctx.params;
-  // 해당 id를 가진 포스트가 몇 번쨰인지 확인한다.
-  const index = posts.findIndex(post => post.id.toString() === id);
-  // 포스트가 없으면 오류를 반환한다.
-  if (index === -1) {
-    cts.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다',
-    };
-    return;
-  }
-  // index 번쨰 아이템을 제거한다.
-  posts.splice(index, 1);
-  ctx.status = 204; // no content
-};
-
-/* 포스트 수정(교체)
-PUT /api/posts/:id
-{ title, body }
-*/
-exports.replace = ctx => {
-  const { id } = ctx.params;
-  const index = posts.findIndex(post => post.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다',
-    };
-    return;
-  }
-  // 전체 객체를 덮어 씌운다.
-  // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만든다.
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
-
-/* 포스트 수정 (특정 필드 변경)
-PATCH /api/posts/:id
-{ title, body }
-*/
-exports.update = ctx => {
-  // PATCH 메서드는 주어진 필드만 교체한다.
-  const { id } = ctx.params;
-  const index = posts.findIndex(post => post.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다',
-    };
-    return;
-  }
-  posts[index] = {
-    ...post[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
+export const remove = ctx => {};
+export const update = ctx => {};
