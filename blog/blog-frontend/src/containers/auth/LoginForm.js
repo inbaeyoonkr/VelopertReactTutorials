a9bfcd54751dc6e02cb0a94488e21d4d3bce7300
ringo2455 <iby2455@gmail.com>
2019-11-27 19:38:27 +0900

    회원가입 실패 에러 메세지 출력 로직 만들기

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
@@ -6,6 +6,7 @@ import { changeField, initializeForm, login } from '../../modules/auth';
 import { check } from '../../modules/user';
 
 const LoginForm = ({ history }) => {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
@@ -37,6 +38,7 @@ const LoginForm = ({ history }) => {
     if (authError) {
       console.log('오류 발생');
       console.log(authError);
+      setError('로그인 실패');
       return;
     }
     if (auth) {
@@ -57,6 +59,7 @@ const LoginForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     ></AuthForm>
   );
 };
