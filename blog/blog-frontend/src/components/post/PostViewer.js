f640348f8aba0230580759b3af0e389b25806cf5
ringo2455 <iby2455@gmail.com>
2019-11-29 18:23:12 +0900

    PostViewer에 props 적용

--- blog/blog-frontend/src/components/post/PostViewer.js
+++ blog/blog-frontend/src/components/post/PostViewer.js
@@ -47,26 +47,34 @@ const PostContent = styled.div`
   color: ${palette.gray[8]};
 `;
 
-const PostViewer = () => {
+const PostViewer = ({ post, error, loading }) => {
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewerBlock>존재하지 않는 포스터입니다.</PostViewerBlock>;
+    }
+    return <PostViewer>오류 발생</PostViewer>;
+  }
+  if (loading || !post) {
+    return null;
+  }
+  const { title, body, tags, user, publishedDate } = post;
   return (
     <PostViewerBlock>
       <PostHead>
-        <h1>제목</h1>
+        <h1>{title}</h1>
         <SubInfo>
           <span>
-            <b>tester</b>
+            <b>{user.username}</b>
           </span>
-          <span>{new Date().toLocaleDateString()}</span>
+          <span>{new Date(publishedDate).toLocaleDateString()}</span>
         </SubInfo>
         <Tags>
-          <div className="tag">#태그1</div>
-          <div className="tag">#태그2</div>
-          <div className="tag">#태그3</div>
+          {tags.map(tag => (
+            <div className="tag">#{tag}</div>
+          ))}
         </Tags>
       </PostHead>
-      <PostContent
-        dangerouslySetInnerHTML={{ __html: '<p>HTML <b>내용</b>입니다<p>' }}
-      ></PostContent>
+      <PostContent dangerouslySetInnerHTML={{ __html: body }}></PostContent>
     </PostViewerBlock>
   );
 };
