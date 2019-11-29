0f80e28db4bf3b844bcb6de025c57abf99128658
ringo2455 <iby2455@gmail.com>
2019-11-29 19:15:14 +0900

    listPosts API 작성

--- blog/blog-frontend/src/lib/api/posts.js
+++ blog/blog-frontend/src/lib/api/posts.js
@@ -1,6 +1,12 @@
+import qs from 'qs';
 import client from './client';
 
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
 
 export const readPost = id => client.get(`/api/posts/${id}`);
+
+export const listPosts = ({ username, tag, page }) => {
+  const queryString = qs.stringify({ username, tag, page });
+  return client.get(`/api/posts/${queryString}`);
+};
