1cf36956df0a4539908331e1af0823e9154fa38b
ringo2455 <iby2455@gmail.com>
2019-11-29 17:35:48 +0900

    PostViewer 컴포넌트 작성

--- blog/blog-frontend/src/containers/common/HeaderContainer.js
+++ blog/blog-frontend/src/containers/common/HeaderContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Header from '../../components/common/Header';
 import { logout } from '../../modules/user';
+
 const HeaderContainer = () => {
   const { user } = useSelector(({ user }) => ({
     user: user.user,
