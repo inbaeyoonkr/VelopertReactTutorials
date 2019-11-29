349d9f44c88abafed5ae6a6af8b1ec54c14b83c2
ringo2455 <iby2455@gmail.com>
2019-11-29 18:57:52 +0900

    SubInfo, Tags 컴포넌트 분리

--- blog/blog-frontend/src/components/post/PostViewer.js
+++ blog/blog-frontend/src/components/post/PostViewer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Responsive from '../common/Responsive';
+
 import palette from '../../lib/styles/palette';
 
 const PostViewerBlock = styled(Responsive)`
