27964108d61dca2e43407f4e9b0d740b5a9174f9
ringo2455 <iby2455@gmail.com>
2019-12-03 16:55:05 +0900

    27

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -99,6 +99,7 @@ export const write = async ctx => {
   }
 };
 
+// html 을 없애고 내용이 너무 길으면 200자로 제한시키는 함수
 const removeHtmlAndShorten = body => {
   const filtered = sanitizeHtml(body, {
     allowedTags: [],
@@ -194,7 +195,6 @@ export const update = async ctx => {
   if (nextData.body) {
     nextData.body = sanitizeHtml(nextData.body);
   }
-
   try {
     const post = await Post.findByIdAndUpdate(id, nextData, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
